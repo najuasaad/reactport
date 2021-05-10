2569cfc18b400470adeb56d1ae4c2dfa8aa99494
Najua Saad <najuasaad@gmail.com>
2021-05-10 17:37:30 -0500

    react app created, rough outline

---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+// import './App.css';
+
+// pages 
+import About from "./pages/About";
+import Portfolio from "./pages/Portfolio";
+import Contact from "./pages/Contact"
+
+// components
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import Navbar from "./components/Navbar"
+import Wrapper from "./components/Wrapper";
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Navbar />
+        <Wrapper>
+          <Route exact path="/" component={About} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/portfolio" component={Portfolio} />
+          <Route exact path="/contact" component={Contact} />
+        </Wrapper>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
