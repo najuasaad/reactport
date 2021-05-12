730284f06182d9c19cb23c43874d8fa4c81d4ed6
Najua Saad <najuasaad@gmail.com>
2021-05-11 21:22:35 -0500

    mvp

--- src/components/Project/index.js
+++ src/components/Project/index.js
@@ -1,16 +1,39 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faFileCode, faWindowRestore } from '@fortawesome/free-solid-svg-icons'
+import user from "../images/userdirectory.jpg"
+import study from "../images/studybuddy.jpg"
+import prog from "../images/progressivebudget.jpg"
+import note from "../images/notetaker.jpg"
+import lunchable from "../images/lunchables.jpg"
+import fit from "../images/fitnesstracker.jpg"
 
 function Project(props) {
+  console.log(props.image)
+
+  const imgSrc = () => {
+    if (props.project === "USER DIRECTORY") {
+      return user
+    } else if (props.project === "STUDY BUDDY") {
+      return study
+    } else if (props.project === "LUNCHABLES") {
+      return lunchable
+    } else if (props.project === "NOTE TAKER") {
+      return note
+    } else if (props.project === "PROGRESSIVE BUDGET") {
+      return prog
+    } else {
+      return fit
+    }
+  }
+
   return (
-    <div className="carousel-item active">
-      <img src={props.image} class="d-block w-100" alt={props.project}/>
-      <div className="carousel-caption d-none d-md-block">
-        <h5>{props.description}</h5>
-        <a href={props.repo}><FontAwesomeIcon icon={faFileCode} /></a>
-        <a href={props.deployed}><FontAwesomeIcon icon={faWindowRestore} /></a>
-      </div>
+    <div className="card projectCard border-0 col-5 mx-auto" /* style="width: 18rem;" */>
+        <p className="card-text orange text-center mt-0 title">{props.project}</p>
+        <p className="card-text orange text-center mt-0">{props.description}</p>
+      <img src={imgSrc()} className="card-img-top projImage" alt={props.project}/>
+        <a className= "position-relative codeIcon" href={props.repo}><FontAwesomeIcon icon={faFileCode} className="yicon"/></a>
+        <a className= "position-relative depIcon mb-0" href={props.deployed}><FontAwesomeIcon icon={faWindowRestore} className="yicon"/></a>
     </div>
   )
 }
--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -2,20 +2,19 @@ import React from "react";
 import myProjects from "../components/myprojects.json"
 
 import Project from "../components/Project"
-import CarouselButtons from "../components/carouselbuttons"
+
 
 function Portfolio() {
   return (
     <div className="container">
-      <div id="carouselExampleCaptions" className="carousel slide" data-ride="carousel">
-        <ol class="carousel-indicators">
-          <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
-        </ol>
-        <div class="carousel-inner">
-          
-          <Project />
-          <CarouselButtons />
-        </div>
+      <div className="row">
+        <p className="text-center mx-auto orange">(click on the icons for the repo and deployed versions)</p>
+      </div>
+      
+      <div className="row">
+        {myProjects.map((proj) => (
+          <Project key={proj.project} {...proj}/>
+        ))}
       </div>
     </div>
   )
